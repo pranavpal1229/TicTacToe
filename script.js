21e268dec692a5f9a4ef352063eb4e9a18551bea
Pranav <pranavpal@uchicago.edu>
2024-08-15 00:17:50 -0500

    fininshed proj

--- script.js
+++ script.js
@@ -1,65 +1,114 @@
+let square_1 = document.querySelector("#button1");
+let square_2 = document.querySelector("#button2");
+let square_3 = document.querySelector("#button3");
+let square_4 = document.querySelector("#button4");
+let square_5 = document.querySelector("#button5");
+let square_6 = document.querySelector("#button6");
+let square_7 = document.querySelector("#button7");
+let square_8 = document.querySelector("#button8");
+let square_9 = document.querySelector("#button9");
+let turn_header = document.querySelector("#turn-header");
+
+let squareList = [
+    { element: square_1, coordinates: [0, 0] },
+    { element: square_2, coordinates: [0, 1] },
+    { element: square_3, coordinates: [0, 2] },
+    { element: square_4, coordinates: [1, 0] },
+    { element: square_5, coordinates: [1, 1] },
+    { element: square_6, coordinates: [1, 2] },
+    { element: square_7, coordinates: [2, 0] },
+    { element: square_8, coordinates: [2, 1] },
+    { element: square_9, coordinates: [2, 2] }
+];
+
 const gameBoard = function(){
-    let board = [['', '', ''], ['', '',''], ['','','']]
-    return {board}
-}() //initializes the board immediately
+    let board = [['', '', ''], ['', '',''], ['','','']];
+    return { board };
+}();
 
 let gameLogic = {
     currentPlayer: 'Player 1',
-    players: ['Player 1', 'Player 2'],
+    currentVal: 'X',
     total_moves : 0,
+    gameOver: false,  // Game over flag
 
     checkWinner : function(){
-        const board = gameBoard.board
-        let i = 0
-        for(let j = 0; j < 3; j++){
-            if(board[j][i] == board[j][i + 1] && board[j][i] == board[j][i + 2]){
-                return board[j][i]
-        }
-     }
+        const board = gameBoard.board;
+        let i = 0;
 
-        for(let j = 0; j < 3; j++){
-            if(board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j]){
-                return board[j][i]
+        // Check rows
+        for (let j = 0; j < 3; j++) {
+            if (board[j][i] == board[j][i + 1] && board[j][i] == board[j][i + 2] && board[j][i] !== '') {
+                return board[j][i];
             }
         }
 
-        if(board[i][i] == board[i + 1][i + 1] && board[0][0] == board[2][2]){
-            return board[0][0]
+        // Check columns
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == board[i + 1][j] && board[i][j] == board[i + 2][j] && board[i][j] !== '') {
+                return board[i][j];
+            }
         }
 
-        if(board[0][2] == board[[1][1]] && board[1][1] == board[2][0]){
-            return board[1][1]
+        // Check diagonals
+        if (board[i][i] == board[i + 1][i + 1] && board[i][i] == board[2][2] && board[i][i] !== '') {
+            return board[i][i];
         }
 
-        return "None"
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] !== '') {
+            return board[1][1];
+        }
+
+        return "None";
     },
 
-    playMove : function(index, val){
-        let i = index[0]
-        let j = index[1]
-        gameBoard.board[i][j] = val
-        if (this.checkWinner() == "X" || this.checkWinner() == "O"){
-            console.log('hi')
+    playMove : function(index, val, element){
+        if (this.gameOver) return;  // Prevent further moves if the game is over
+
+        let i = index[0];
+        let j = index[1];
+        if (gameBoard.board[i][j] == "X" || gameBoard.board[i][j] == "O") {
+            return;
+        }
+
+        gameBoard.board[i][j] = val;
+        this.total_moves += 1;
+        let head = document.createElement("h1");
+        head.textContent = val;
+        element.appendChild(head);
+
+        const winner = this.checkWinner();
+        if (winner === "X" || winner === "O") {
+            turn_header.textContent = `${this.currentPlayer} Wins!`;
+            this.gameOver = true;  // Set game over flag
+            return;
+        }
+
+        if (this.currentPlayer == "Player 1") {
+            this.currentPlayer = "Player 2";
+            this.currentVal = "O";
+            turn_header.textContent = "Player 2 Turn";
+        } else {
+            this.currentPlayer = "Player 1";
+            this.currentVal = "X";
+            turn_header.textContent = "Player 1 Turn";
+        }
+
+        if (this.total_moves == 9 && !this.gameOver) {
+            turn_header.textContent = "Draw!";
+            this.gameOver = true;  // Set game over flag
         }
     }
 }
-board = gameBoard.board
-gameLogic.playMove([0,0], 'X')
-gameLogic.playMove([1,1], 'O')
-gameLogic.playMove([2,2], 'X')
-gameLogic.playMove([0,1], 'X')
-gameLogic.playMove([0,2], 'X')
 
+squareList.forEach(square => {
+    square.element.addEventListener("click", () => gameLogic.playMove(square.coordinates, gameLogic.currentVal, square.element));
+});
 
-
-console.log(gameLogic.checkWinner())
-console.log(board)
-
-
-function createPlayer(name, symb){
-    return {name, symb}
+function createPlayer(name, symb) {
+    return { name, symb };
 }
 
-player1 = createPlayer("Player 1", "X")
-player2  = createPlayer("player 2", "O")
+let player1 = createPlayer("Player 1", "X");
+let player2 = createPlayer("Player 2", "O");
 
